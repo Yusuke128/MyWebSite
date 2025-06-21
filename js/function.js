d5049517bb179592566a0b834d56a13788b341b4
Yusuke128 <50142020+Yusuke128@users.noreply.github.com>
2025-06-22 01:05:21 +0900

    all custiomize complete ver.1

--- js/function.js
+++ js/function.js
@@ -1,8 +1,8 @@
-// function.js の完成コード
-
 $(document).ready(function () {
-  // 1. 実績スライダー (既存のコード)
-  $(".slider").slick({
+  let slider_class = $(".website,.design");
+
+  // 実績部分の無限スライダー
+  slider_class.slick({
     autoplay: true,
     autoplaySpeed: 0,
     speed: 10000,
@@ -16,15 +16,29 @@ $(document).ready(function () {
     variableWidth: true,
     responsive: [
       {
-        breakpoint: 750,
+        breakpoint: 796,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 1,
         },
       },
     ],
   });
+  // スライダーのタブによる表示切り替え
+  $(".works_button").on("click", function () {
+    let tabId = $(this).data("tab"); //HTMLでdata-tabで設定している値を取得
+    $(".works_button").removeClass("active");
+    $(this).addClass("active");
+    // 一度全てのactiveクラスを削除しクリックした要素にactiveクラスを付与する
 
-  //ハンバーガーメニュー (既存のコード)
+    $(".slidebox").removeClass("active");
+    $("." + tabId).addClass("active");
+  });
+
+  if ($(this).hasClass("active")) {
+    slider_class.slick("setPosition");
+  }
+
+  //ハンバーガーメニュー
   const hum = $(".humberger");
   const header = $(".top_header");
   const body = $("body");
@@ -63,4 +77,65 @@ $(document).ready(function () {
   animationTargets.each(function () {
     animationObserver.observe(this);
   });
+  /*
+  // detailタグのスムーズな開閉処理
+  // アニメーションの時間とイージング
+  const animTiming = {
+    duration: 500,
+    easing: "ease-in-out",
+  };
+
+  // アコーディオンを閉じるときのキーフレーム
+  const closingAnimation = (content) => [
+    {
+      height: content.offsetHeight + "px",
+      opacity: 1,
+    },
+    {
+      height: 0,
+      opacity: 0,
+    },
+  ];
+
+  // アコーディオンを開くときのキーフレーム
+  const openingAnimation = (content) => [
+    {
+      height: 0,
+      opacity: 0,
+    },
+    {
+      height: content.offsetHeight + "px",
+      opacity: 1,
+    },
+  ];
+
+  document.querySelectorAll(".skillsContentBox").forEach(function (el) {
+    const summary = el.querySelector(".skills_name");
+    const content = el.querySelector(".skills_content");
+    summary.addEventListener("click", (event) => {
+      // デフォルトの挙動を無効化
+      event.preventDefault();
+      // detailsのopen属性を判定
+      if (el.getAttribute("open") !== null) {
+        // アコーディオンを閉じるときの処理
+        const closingAnim = content.animate(
+          closingAnimation(content),
+          animTiming
+        );
+
+        closingAnim.onfinish = () => {
+          // アニメーションの完了後にopen属性を取り除く
+          el.removeAttribute("open");
+        };
+      } else {
+        // open属性を付与
+        el.setAttribute("open", "true");
+        // アコーディオンを開くときの処理
+        const openingAnim = content.animate(
+          openingAnimation(content),
+          animTiming
+        );
+      }
+    });
+  });*/
 });
